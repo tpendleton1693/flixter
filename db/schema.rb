1789837cc1185fc0efbd0f43cce7787cc0cbfb48
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-10-16 22:04:12 -0700

    Added section re-ordering

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017030234) do
+ActiveRecord::Schema.define(version: 20151017044418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,9 +56,11 @@ ActiveRecord::Schema.define(version: 20151017030234) do
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "row_order"
   end
 
   add_index "sections", ["course_id"], name: "index_sections_on_course_id", using: :btree
+  add_index "sections", ["row_order"], name: "index_sections_on_row_order", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
